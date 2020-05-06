99e135232d0e937ac4ba648b460ddfb30379a62f
iamdrewkirkland <drew.kirkland@gmail.com>
2020-05-06 09:58:50 -0400

    Login and register pages are functionally linked. Both pages have been refactored to use MaterialUI.

---
+++ src/components/AppState.js
@@ -0,0 +1,51 @@
+// component resposible for checking and controlling the application state.
+import React, { useState, useEffect } from "react";
+import SignIn from "./auth/SignIn";
+import Register from "./auth/Register";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import StaticHead from "./auth/StaticHead";
+
+const useStyles = makeStyles((theme) => ({
+paper: {
+  display: "grid",
+  // flexFlow: "column nowrap",
+  // alignItems: "center",
+  marginTop: theme.spacing(10),
+  padding: theme.spacing(2),
+},
+}));
+
+export default () => {
+  // set variable for styles
+  const classes = useStyles()
+
+  //state hooks
+  const [components, setComponents] = useState();
+  const [activeView, setActiveView] = useState("signIn");
+  //components with state function passed as key-value pair
+  const signIn = () => <SignIn view={setActiveView} />;
+  const register = () => <Register view={setActiveView} />;
+
+  // effect hooks to change the component
+  useEffect(() => {
+    if (activeView === "signIn") {
+      setComponents(signIn);
+    } else if (activeView === "register") {
+      setComponents(register);
+    }
+  }, [activeView]);
+
+  // jsx fragment container to hold current component
+  return (
+    <>
+      <Container className="componentView" maxWidth="xs">
+       <Paper className={classes.paper}>
+         <StaticHead />
+         {components}
+       </Paper>
+      </Container>
+    </>
+  );
+};
--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,14 +1,8 @@
 import React from "react";
-import Login from "./Login";
-import Register from "./Register";
-import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import Avatar from "@material-ui/core/Avatar";
-import LockOutlined from "@material-ui/icons/LockOutlined";
 import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
-import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 
 //function to clear login form and render register form
@@ -16,85 +10,56 @@ import { makeStyles } from "@material-ui/core/styles";
 
 //theme and layout control
 const useStyles = makeStyles((theme) => ({
-  container: {
-    // padding: theme.spacing(2),
-  },
-  avatar: {
-      backgroundColor: theme.palette.primary.main
-  },
-  paper: {
-    display: "flex",
-    flexFlow: "column nowrap",
-    alignItems: "center",
-    marginTop: theme.spacing(10),
-    padding: theme.spacing(2)
-  },
   form: {
-    marginTop: theme.spacing(2)
+    marginTop: theme.spacing(2),
   },
-  fakeLink:{
-    marginTop:theme.spacing(2),
-    alignSelf: "flex-end"   
-  }
+  fakeLink: {
+    marginTop: theme.spacing(2),
+    alignSelf: "flex-end",
+  },
 }));
 
-export default ({ toggle }) => {
+export default ({ view, toggle }) => {
   const classes = useStyles();
   return (
     <>
-      <Container className={classes.container} maxWidth="xs">
-        <Paper className={classes.paper} elevation={3}>
-          <Avatar className={classes.avatar} variant="rounded" alt="login">
-            <LockOutlined />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            mech köllection
-          </Typography>
-          <Typography component="p">please sign in</Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              toggle={toggle}
-            >
-              Sign In
-            </Button>
-          </form>
-          <Link
-            component="button"
-            className={classes.fakeLink}
-            onClick={() => {
-              console.log("Register function go here.");
-            }}
-          >
-            {"Don't have an account? Register Here."}
-          </Link>
-        </Paper>
-      </Container>
+      <Typography component="p">please sign in</Typography>
+      <form className={classes.form} noValidate>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <Button type="submit" fullWidth variant="contained" color="primary">
+          Sign In
+        </Button>
+      </form>
+      <Link
+        component="button"
+        className={classes.fakeLink}
+        onClick={() => {
+          view("register");
+        }}
+      >
+        {"Don't have an account? Register here."}
+      </Link>
     </>
   );
 };
