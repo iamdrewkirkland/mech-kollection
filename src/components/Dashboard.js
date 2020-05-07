1366c92d428848fcc269c9f778b17c26ec2c5695
iamdrewkirkland <drew.kirkland@gmail.com>
2020-05-06 23:15:07 -0400

    Setup initial structure of Dashboard component.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,96 @@
+import React from "react";
+import clsx from "clsx";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
+// drawer width for theme and layout control
+const drawerWidth = 210;
 
-// invisible appbar with centered logo text and far right fake social links
+// theme and layout control
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  drawer: {
+    width: drawerWidth,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    marginLeft: drawerWidth,
+  },
+  container: {
+    paddingTop: theme.mixins.toolbar,
+  },
 
-// 
+  paper: {
+    padding: theme.spacing(2),
+    display:"flex",
+    overflow:"auto"
+  },
+  fixedHeight: {
+    height: 350,
+  },
+}));
+
+// invisible appbar with centered logo text
+const Dashboard = () => {
+  // theme and layout control variables
+  const classes = useStyles();
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+  return (
+    <>
+      <div className={classes.root}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <Typography component="h1" variant="h6">
+              mech köllection
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.appBarSpacer} />
+        <Drawer variant="permanent" className={classes.drawer}>
+          <List>
+            <ListItem>1</ListItem>
+            <ListItem>2</ListItem>
+            <ListItem>3</ListItem>
+          </List>
+        </Drawer>
+        <main>
+          <Container maxWidth="lg" className={classes.container}>
+            <Grid container spacing={2}>
+              <Grid item lg={12}>
+                <Paper className={fixedHeightPaper}>
+                  This is the first grid item. BuildList goes here.
+                </Paper>
+              </Grid>
+              <Grid item lg={7}>
+                <Paper className={fixedHeightPaper}>
+                  This is the second grid item. ResultsList goes here.
+                </Paper>
+              </Grid>
+              <Grid item lg={4}>
+                <Paper className={fixedHeightPaper}>
+                  This is the third grid item. FriendList goes here.
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </main>
+      </div>
+    </>
+  );
+};
+// left side vertical tabs for static navigation
+
+// main container with collection, friends, and test results sections
+
+export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import AppState from "./components/AppState";
+// import AppState from "./components/AppState";
+import Dashboard from "./components/Dashboard";
 
-ReactDOM.render(<AppState />, document.getElementById("root"));
+ReactDOM.render(<Dashboard />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
