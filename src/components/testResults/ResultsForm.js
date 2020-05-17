a2f90ca269c13531567fd7b29666df28c3df842d
iamdrewkirkland <drew.kirkland@gmail.com>
2020-05-17 19:42:35 -0400

    Results form and list now captures all inputs properly.

--- src/components/testResults/ResultsForm.js
+++ src/components/testResults/ResultsForm.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useContext, useState } from "react";
 import {
   Grid,
   Typography,
@@ -19,146 +19,149 @@ import { ResultsContext } from "./ResultsProvider";
 import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 import MomentUtils from "@date-io/moment";
 
-export const ResultsForm = React.memo(
-  ({ result, toggleForm, setResult, myCollection }) => {
-    const buildId = useRef(null);
-    const wpm = useRef(null);
-    const [personalBest, setPersonalBest] = useState(false);
-    const [selectedDate, handleDateChange] = useState(new Date());
-    const [radioValue, setRadioValue] = useState(null);
-    const [error, setError] = useState(false);
+export const ResultsForm = React.memo(({ toggleForm, myCollection }) => {
+  const [buildId, setBuildId] = useState(null);
+  const [wpm, setWpm] = useState(null);
+  const [personalBest, setPersonalBest] = useState(false);
+  const [selectedDate, handleDateChange] = useState(new Date());
+  const [radioValue, setRadioValue] = useState(null);
+  const [error, setError] = useState(false);
 
-    const { addResults } = useContext(ResultsContext);
-    const websitesArray = [
-      "10fastfingers",
-      "typetest.io",
-      "typeracer",
-      "typing.works",
-    ];
-    function toggleBest() {
-      setPersonalBest(!personalBest);
-    }
+  const { addResults } = useContext(ResultsContext);
+  
+  //object containing values of user inputs 
+  const newResultObject = {
+    buildId: buildId,
+    date: selectedDate,
+    wpm: parseInt(wpm),
+    pb: personalBest,
+    website: radioValue,
+  };
 
-    function handleChange() {
-      const newResultObject = {
-        buildId: buildId.current.value,
-        date: selectedDate,
-        wpm: parseInt(wpm.current.value),
-        pb: personalBest,
-        website: radioValue,
-      };
-      setResult(newResultObject);
-    }
-    function handleSubmit(e) {
-      e.preventDefault();
-      if (radioValue === null) {
-        setError(true);
-      } else {
-        addResults(result);
-        toggleForm();
-      }
-    }
+  //add or remove websites here
+  const websitesArray = [
+    "10fastfingers",
+    "typetest.io",
+    "typeracer",
+    "typing.works",
+  ];
 
-    //function to check if property exsists and is not null
-    function inputCheck(buildObject, prop) {
-      if (buildObject.hasOwnProperty(prop) && buildObject[prop] !== "") {
-        return buildObject[prop];
-      }
-    }
-
-    function handleRadioChange(e) {
-      setRadioValue(e.target.value);
-      setError(false);
-    }
-    return (
-      <>
-        <Typography variant="h4">Results Info</Typography>
-        <Grid container spacing={2}>
-          <FormControl component="fieldset" error={error}>
-            <Grid>
-              <InputLabel id="build">Build</InputLabel>
-              <Select
-                labelId="build"
-                onChange={handleChange}
-                inputRef={buildId}
-              >
-                {myCollection.map((build) => (
-                  <MenuItem
-                    key={
-                      inputCheck(build, "name")
-                        ? `${build.name}`
-                        : `${build.caseName}`
-                    }
-                    value={build.id}
-                  >
-                    {inputCheck(build, "name")
-                      ? `${build.name}`
-                      : `${build.caseName}`}
-                  </MenuItem>
-                ))}
-              </Select>
-            </Grid>
-            <Grid>
-              <MuiPickersUtilsProvider utils={MomentUtils}>
-                <DateTimePicker
-                  value={selectedDate}
-                  onChange={handleDateChange}
-                  disableFuture={true}
-                />
-              </MuiPickersUtilsProvider>
-            </Grid>
-            <Grid item>
-              <TextField label="WPM" inputRef={wpm} onChange={handleChange} />
-            </Grid>
-            <Grid item>
-              <FormControlLabel
-                label="Personal Best?"
-                control={
-                  <Switch
-                    onChange={() => {
-                      toggleBest();
-                      handleChange();
-                    }}
-                  />
-                }
-              />
-            </Grid>
-
-            <Grid>
-              <FormLabel>Website</FormLabel>
-              <RadioGroup
-                name="website"
-                value={radioValue}
-                onChange={handleRadioChange}
-              >
-                {websitesArray.map((website) => {
-                  return (
-                    <FormControlLabel
-                      key={website}
-                      label={website}
-                      value={website}
-                      labelPlacement="end"
-                      control={<Radio />}
-                    />
-                  );
-                })}
-              </RadioGroup>
-              <FormHelperText color="secondary">
-                {error ? "Please selection an option." : null}
-              </FormHelperText>
-            </Grid>
-          </FormControl>
-
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            onClick={handleSubmit}
-          >
-            ADD RESULT
-          </Button>
-        </Grid>
-      </>
-    );
+  function toggleBest() {
+    setPersonalBest(!personalBest);
   }
-);
+
+  function handleWpmChange(e) {
+    setWpm(e.target.value);
+  }
+  function handleBuildChange(e) {
+    setBuildId(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (radioValue === null) {
+      setError(true);
+    } else {
+      debugger;
+      addResults(newResultObject);
+      toggleForm();
+    }
+  }
+
+  //function to check if property exsists and is not null
+  function inputCheck(buildObject, prop) {
+    if (buildObject.hasOwnProperty(prop) && buildObject[prop] !== "") {
+      return buildObject[prop];
+    }
+  }
+
+  function handleRadioChange(e) {
+    setRadioValue(e.target.value);
+    setError(false);
+  }
+  return (
+    <>
+      <Typography variant="h4">Results Info</Typography>
+      <Grid container spacing={2}>
+        <FormControl component="fieldset" error={error}>
+          <Grid>
+            <InputLabel id="build">Build</InputLabel>
+            <Select labelId="build" onChange={handleBuildChange}>
+              {myCollection.map((build) => (
+                <MenuItem
+                  key={
+                    inputCheck(build, "name")
+                      ? `${build.name}`
+                      : `${build.caseName}`
+                  }
+                  value={build.id}
+                >
+                  {inputCheck(build, "name")
+                    ? `${build.name}`
+                    : `${build.caseName}`}
+                </MenuItem>
+              ))}
+            </Select>
+          </Grid>
+          <Grid>
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+              <DateTimePicker
+                value={selectedDate}
+                onChange={handleDateChange}
+                disableFuture={true}
+              />
+            </MuiPickersUtilsProvider>
+          </Grid>
+          <Grid item>
+            <TextField label="WPM" value={wpm} onChange={handleWpmChange} />
+          </Grid>
+          <Grid item>
+            <FormControlLabel
+              label="Personal Best?"
+              control={
+                <Switch
+                  onChange={() => {
+                    toggleBest();
+                  }}
+                />
+              }
+            />
+          </Grid>
+
+          <Grid>
+            <FormLabel>Website</FormLabel>
+            <RadioGroup
+              name="website"
+              value={radioValue}
+              onChange={handleRadioChange}
+            >
+              {websitesArray.map((website) => {
+                return (
+                  <FormControlLabel
+                    key={website}
+                    label={website}
+                    value={website}
+                    labelPlacement="end"
+                    control={<Radio />}
+                  />
+                );
+              })}
+            </RadioGroup>
+            <FormHelperText color="secondary">
+              {error ? "Please selection an option." : null}
+            </FormHelperText>
+          </Grid>
+        </FormControl>
+
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          ADD RESULT
+        </Button>
+      </Grid>
+    </>
+  );
+});
