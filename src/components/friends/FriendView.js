71f19b419ba2c6878128c7610279b045a6a69032
iamdrewkirkland <drew.kirkland@gmail.com>
2020-05-18 10:11:43 -0400

    Dashboard view refactored so that each component has header with action buttons.

--- src/components/friends/FriendView.js
+++ src/components/friends/FriendView.js
@@ -1,25 +1,76 @@
 import React, { useState } from "react";
-import { Button } from "@material-ui/core";
+import {
+  makeStyles,
+  AppBar,
+  Toolbar,
+  Tooltip,
+  Typography,
+  Fab,
+} from "@material-ui/core";
 import FriendForm from "./FriendForm";
 import FriendList from "./FriendList";
+import AddRoundedIcon from "@material-ui/icons/AddRounded";
+import ArrowBackRoundedIcon from "@material-ui/icons/ArrowBackRounded";
 
+const useStyles = makeStyles((theme) => ({
+  flexGrow: {
+    flexGrow: 1,
+  },
+  cardAppBar: {
+    height: 75,
+    padding: theme.spacing(),
+  },
+}));
 
-export default function FriendsView({currentUserId}) {
+export default function FriendsView({ currentUserId }) {
+  const classes = useStyles();
   const [showForm, toggleShowForm] = useState(false);
 
   return (
     <>
-      {showForm ? <FriendForm toggleForm={toggleShowForm} currentUserId={currentUserId} /> : <FriendList currentUserId={currentUserId} />}
-
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => {
-          toggleShowForm(!showForm);
-        }}
-      >
-        {showForm ? "View Friends" : "Add Friend"}
-      </Button>
+      <div className={classes.flexGrow}>
+        <AppBar
+          position="static"
+          color="primary"
+          elevation={0}
+          className={classes.cardAppBar}
+        >
+          <Toolbar>
+            <Typography variant="h6" className={classes.flexGrow}>
+              Friends
+            </Typography>
+            {showForm ? (
+              <Tooltip title="Back">
+                <Fab
+                  color="secondary"
+                  onClick={() => {
+                    toggleShowForm();
+                  }}
+                >
+                  <ArrowBackRoundedIcon />
+                </Fab>
+              </Tooltip>
+            ) : (
+              <Tooltip title="Add New Friend">
+                <Fab
+                  size="small"
+                  color="action"
+                  onClick={() => {
+                    toggleShowForm();
+                  }}
+                >
+                  <AddRoundedIcon />
+                </Fab>
+              </Tooltip>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+      {showForm ? (
+        <FriendForm toggleForm={toggleShowForm} currentUserId={currentUserId} />
+      ) : (
+        <FriendList currentUserId={currentUserId} />
+      )}
     </>
   );
 }
